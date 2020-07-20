03a10762a1e3d520e7beff05703e7c524dbb1c80
Zihan Chen <vczh@163.com>
2020-07-20 15:30:13 -0700

    ...

--- Source/Reflection/GuiTypeDescriptorBuilder_Struct.h
+++ Source/Reflection/GuiTypeDescriptorBuilder_Struct.h
@@ -1,9 +1,6 @@
-/***********************************************************************
-Vczh Library++ 3.0
-Developer: Zihan Chen(vczh)
-Framework::Reflection
-	
-Interfaces:
+/*/***********************************************************************
+Author: Zihan Chen (vczh)
+Licensed under https://github.com/vczh-libraries/License
 ***********************************************************************/
  
 #ifndef VCZH_REFLECTION_GUITYPEDESCRIPTORBUILDER_STRUCT
--- Source/Reflection/GuiTypeDescriptorReflection.cpp
+++ Source/Reflection/GuiTypeDescriptorReflection.cpp
@@ -1,3 +1,8 @@
+/***********************************************************************
+Author: Zihan Chen (vczh)
+Licensed under https://github.com/vczh-libraries/License
+***********************************************************************/
+
 #include <limits.h>
 #include <float.h>
 #include "GuiTypeDescriptorReflection.h"
