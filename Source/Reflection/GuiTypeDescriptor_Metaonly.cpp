5f800e9cf463d9e50fd60f5964ff840f2115aaf8
vczh <vczh@outlook.com>
2021-10-04 13:34:22 -0700

    Update GuiTypeDescriptor_Metaonly.cpp

--- Source/Reflection/GuiTypeDescriptor_Metaonly.cpp
+++ Source/Reflection/GuiTypeDescriptor_Metaonly.cpp
@@ -13,10 +13,38 @@ namespace vl
 		{
 			using namespace collections;
 
-			using ContextFreeReader = stream::internal::ContextFreeReader;
-			using ContextFreeWriter = stream::internal::ContextFreeWriter;
-
 #ifndef VCZH_DEBUG_NO_REFLECTION
+
+			struct MetaonlyWriterContext
+			{
+				Dictionary<ITypeDescriptor*, vint>		tdIndex;
+				Dictionary<IMethodInfo*, vint>			miIndex;
+				Dictionary<IPropertyInfo*, vint>		piIndex;
+				Dictionary<IEventInfo*, vint>			eiIndex;
+			};
+
+			struct MetaonlyReaderContext
+			{
+
+			};
+
+			using Reader = stream::internal::Reader<Ptr<MetaonlyReaderContext>>;
+			using Writer = stream::internal::Writer<Ptr<MetaonlyWriterContext>>;
+
+/***********************************************************************
+ITypeInfo
+***********************************************************************/
+
+			class MetaonlyTypeInfo : public Object, public ITypeInfo
+			{
+			protected:
+				Decorator						decorator = TypeDescriptor;
+				TypeInfoHint					hint = TypeInfoHint::Normal;
+				ITypeInfo*						elementType = nullptr;
+				ITypeDescriptor*				typeDecriptor=nullptr;
+				List<Ptr<MetaonlyTypeInfo>>		genericArguments;
+				WString							typeFriendlyName;
+			};
 
 /***********************************************************************
 GenerateMetaonlyTypes
@@ -24,7 +52,9 @@ GenerateMetaonlyTypes
 
 			void GenerateMetaonlyTypes(stream::IStream& outputStream)
 			{
-				ContextFreeWriter writer(outputStream);
+				Writer writer(outputStream);
+				writer.context = MakePtr<MetaonlyWriterContext>();
+
 				Dictionary<WString, ITypeDescriptor*> tds;
 				{
 					auto tm = GetGlobalTypeManager();
@@ -34,6 +64,37 @@ GenerateMetaonlyTypes
 					{
 						auto td = tm->GetTypeDescriptor(i);
 						tds.Add(td->GetTypeName(), td);
+					}
+				}
+				{
+					vint count = tds.Count();
+					for (vint i = 0; i < count; i++)
+					{
+						auto td = tds.Values()[i];
+						writer.context->tdIndex.Add(td, writer.context->tdIndex.Count());
+
+						vint mgCount = td->GetMethodGroupCount();
+						for (vint j = 0; j < mgCount; j++)
+						{
+							auto mg = td->GetMethodGroup(j);
+							vint miCount = mg->GetMethodCount();
+							for (vint k = 0; k < miCount; k++)
+							{
+								writer.context->miIndex.Add(mg->GetMethod(k), writer.context->miIndex.Count());
+							}
+						}
+
+						vint piCount = td->GetPropertyCount();
+						for (vint j = 0; j < piCount; j++)
+						{
+							writer.context->piIndex.Add(td->GetProperty(j), writer.context->piIndex.Count());
+						}
+
+						vint eiCount = td->GetEventCount();
+						for (vint j = 0; j < eiCount; j++)
+						{
+							writer.context->eiIndex.Add(td->GetEvent(j), writer.context->eiIndex.Count());
+						}
 					}
 				}
 				{
@@ -53,7 +114,7 @@ LoadMetaonlyTypes
 
 			void LoadMetaonlyTypes(stream::IStream& inputStream)
 			{
-				ContextFreeReader reader(inputStream);
+				Reader reader(inputStream);
 			}
 
 #endif
