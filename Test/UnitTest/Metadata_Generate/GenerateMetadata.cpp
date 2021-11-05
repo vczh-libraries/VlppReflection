68997b1181a99c870979e4223795bc4ab201a789
vczh <vczh@outlook.com>
2021-11-05 01:49:42 -0700

    Update GenerateMetadata.cpp

--- Test/UnitTest/Metadata_Generate/GenerateMetadata.cpp
+++ Test/UnitTest/Metadata_Generate/GenerateMetadata.cpp
@@ -1,6 +1,7 @@
 #include "../../../Source/Reflection/Reflection/Reflection.h"
 
 using namespace vl;
+using namespace vl::collections;
 using namespace vl::filesystem;
 using namespace vl::stream;
 using namespace vl::reflection;
@@ -41,14 +42,12 @@ TEST_FILE
 			StreamWriter writer(encoderStream);
 			LogTypeManager(writer);
 		}
-#ifdef VCZH_MSVC
-		// TODO: Need to normalize CRLF and LF before comparing
 		{
-			auto first = File(GetTestOutputPath() + REFLECTION_OUTPUT).ReadAllTextByBom();
-			auto second = File(GetTestOutputPath() + L"../Resources/Baseline/" REFLECTION_BASELINE).ReadAllTextByBom();
-			TEST_ASSERT(first == second);
+			List<WString> first, second;
+			File(GetTestOutputPath() + REFLECTION_OUTPUT).ReadAllLinesByBom(first);
+			File(GetTestOutputPath() + L"../Resources/Baseline/" REFLECTION_BASELINE).ReadAllLinesByBom(second);
+			TEST_ASSERT(CompareEnumerable(first, second) == 0);
 		}
-#endif
 		TEST_ASSERT(ResetGlobalTypeManager());
 	});
 }
